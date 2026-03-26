9ae093d3ed265c3d23b3eebfcab1135a6a976dd1
roshannair-04 <roshannair2004@gmail.com>
2026-03-26 23:48:48 +0530

    Started with the basic once again

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,6 @@
+"use strict";
+// alert("hello"); we are using node js not browser
+console.log(3+3);
+let name="Roshan";
+let age=21;
+let isLoggedin=true;
---
+++ 01_basics/variables.js
@@ -0,0 +1,7 @@
+const accountId=144453;
+let accountEmail="rosh@gmail.com";
+var accountPass="12345";
+accountCity="Jaipur"
+
+// accountId=2;
+console.table([accountId,accountEmail,accountCity])
